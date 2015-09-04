a8ad0f1ec98e76d2fbcac9294b6b0b0b34e28b91
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-09-04 17:18:57 +0200

    SSDM-2425 : improved script

--- submit_jobs_template.sh
+++ submit_jobs_template.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 
-# ROOT_FOLDER=/cluster/scratch_xp/public/schmittu
-
-RESOURCE="-R rusage[scratch=50000] -R lustre"
+R="-W 3:00 -R rusage[scratch=100000] -R lustre"
 
 DATA_FOLDER={data_folder}
 WORK_FOLDER={work_folder}
 
-NUM_WORKERS={job_count}
-
-# import fake data:
-# cp tests/data/test_data.txt $DATA_FOLDER/test_data_0.txt
-# cp tests/data/test_data.txt $DATA_FOLDER/test_data_1.txt
+JC={job_count}
 
 
 GROUP={work_folder}
@@ -20,12 +14,12 @@ GROUP={work_folder}
 MSG_FOLDER={message_folder}
 
 
-bsub -o $MSG_FOLDER/check_out -cwd $GROUP -J"check" $RESOURCE -g $GROUP <<EOL
+bsub -o $MSG_FOLDER/check_out -cwd $GROUP -J "check" $R -g $GROUP <<EOL
     pyprophet-cli check --data-folder $DATA_FOLDER \
-                        --data-filename-pattern "*.txt"
+                        --data-filename-pattern "{data_filename_pattern}"
 EOL
 
-bsub -o $MSG_FOLDER/subsample_out -J"subsample[1-$NUM_WORKERS]" -w"done(check)" $RESOURCE -g $GROUP <<EOL
+bsub -o $MSG_FOLDER/subsample_out -J "subsample[1-$JC]" -w "done(check)" $R -g $GROUP <<EOL
      pyprophet-cli subsample --data-folder $DATA_FOLDER \
                              --data-filename-pattern "{data_filename_pattern}" \
                              --working-folder $WORK_FOLDER \
@@ -34,51 +28,60 @@ bsub -o $MSG_FOLDER/subsample_out -J"subsample[1-$NUM_WORKERS]" -w"done(check)" 
                              --sample-factor {sample_factor} \
                              --ignore-invalid-scores \
                              --local-folder \$TMPDIR \
+                             --chunk-size 1000000 \
                              {extra_args_subsample}
 EOL
 
-bsub -o $MSG_FOLDER/learn_out -J"learn" -w"done(subsample)" -g $GROUP <<EOL
+bsub -o $MSG_FOLDER/learn_out -J "learn" -w "done(subsample)" -g $GROUP $R <<EOL
      pyprophet-cli learn     --working-folder $WORK_FOLDER \
                              {extra_args_learn}
 EOL
 
-bsub -o $MSG_FOLDER/apply_weights_out -J"apply_weights[1-$NUM_WORKERS]" -w"done(learn)" $RESOURCE -g $GROUP <<EOL
+bsub -o $MSG_FOLDER/apply_weights_out -J "apply_weights[1-$JC]" -w "done(learn)" $R -g $GROUP <<EOL
      pyprophet-cli apply_weights --data-folder $DATA_FOLDER \
                                  --data-filename-pattern "{data_filename_pattern}" \
                                  --working-folder $WORK_FOLDER \
                                  --job-number \$LSB_JOBINDEX \
                                  --job-count \$LSB_JOBINDEX_END \
                                  --local-folder \$TMPDIR \
+                                 --chunk-size 1000000 \
                                  {extra_args_apply_weights}
 EOL
 
-bsub -o $MSG_FOLDER/scorer_out -J"score[1-$NUM_WORKERS]" -w"done(apply_weights)" $RESOURCE -g $GROUP <<EOL
-     pyprophet-cli score     --data-folder $DATA_FOLDER \
-                             --data-filename-pattern "{data_filename_pattern}" \
-                             --working-folder $WORK_FOLDER \
-                             --job-number \$LSB_JOBINDEX \
-                             --job-count \$LSB_JOBINDEX_END \
-                             --local-folder \$TMPDIR \
-                             --overwrite-results \
-                             {extra_args_score}
+bsub -o $MSG_FOLDER/scorer_out -J "score[1-$JC]" -w "done(apply_weights)" $R -g $GROUP <<EOL
+     pyprophet-cli score --data-folder $DATA_FOLDER \
+                         --data-filename-pattern "{data_filename_pattern}" \
+                         --working-folder $WORK_FOLDER \
+                         --job-number \$LSB_JOBINDEX \
+                         --job-count \$LSB_JOBINDEX_END \
+                         --local-folder \$TMPDIR \
+                         --overwrite-results \
+                         --chunk-size 1000000 \
+                         {extra_args_score}
 EOL
 
 
-bsub -J"error"  -w"exit(score)||exit(check)||exit(subsample)||exit(learn)||exit(apply_weights)" -g $GROUP "echo failed"
-bsub -J"done"  -w"done(score)" -g $GROUP "echo done"
+bsub -oo $MSG_FOLDER/final_out -J "error" \
+     -w "exit(score)||exit(check)||exit(subsample)||exit(learn)||exit(apply_weights)"\
+     -g $GROUP "echo workflow failed"
+
+bsub -oo $MSG_FOLDER/final_out -J "success"\
+     -w "done(score)"\
+     -g $GROUP "echo workflow finished"
 
 # block until done
-bsub -K -w"done(score) || done(error)" -g $GROUP "echo finalized"
+bsub -K -w "done(success) || done(error)" -g $GROUP "echo finalized"
 
 bkill -g $GROUP 0
 
-for FILE in subsample_out learn_out apply_weights_out scorer_out; do
+for FILE in check_out subsample_out learn_out apply_weights_out scorer_out final_out; do
     FULLPATH=$MSG_FOLDER/$FILE
     echo
     if test -f $FULLPATH; then
         echo CONTENT OF $FULLPATH
         echo
-        cat $FULLPATH
+        # indent:
+        cat $FULLPATH | sed 's/^/||     /'
     else
         echo $FULLPATH is empty
     fi
