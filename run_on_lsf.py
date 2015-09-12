ff9621f87bbf7366c7096e3668f0da622e8e142b
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-09-13 00:47:24 +0200

    SSDM-2425 : refac

--- run_on_lsf.py
+++ run_on_lsf.py
@@ -3,16 +3,12 @@ from __future__ import print_function
 
 import tempfile
 import os
-import shutil
 import subprocess
 import threading
 import time
 
 
-def run_workflow(work_folder, result_folder, data_folder, data_filename_pattern, job_count, sample_factor,
-                 extra_args_subsample="", extra_args_learn="",
-                 extra_args_apply_weights="", extra_args_score="", callback=None, logger=None):
-
+def _setup_folders(result_folder, work_folder, logger):
     if result_folder is None:
         user = os.environ.get("USER")
         tmp_root = "/cluster/scratch_xp/public/%s/pyprophet_tmp" % user
@@ -35,13 +31,18 @@ def run_workflow(work_folder, result_folder, data_folder, data_filename_pattern,
         logger.info("result folder at %s", result_folder)
         logger.info("work folder at %s", work_folder)
 
+    return result_folder, work_folder
+
+
+def _write_script(work_folder, logger, settings):
+
     here = os.path.dirname(os.path.abspath(__file__))
     script_template_path = os.path.join(here, "submit_jobs_template.sh")
     template = open(script_template_path, "r").read()
     if logger is not None:
         logger.info("use script template from %s", script_template_path)
 
-    script = template.format(**locals())
+    script = template.format(**settings)
 
     script_path = os.path.join(work_folder, "run.sh")
     with open(script_path, "w") as fp:
@@ -49,6 +50,15 @@ def run_workflow(work_folder, result_folder, data_folder, data_filename_pattern,
 
     if logger is not None:
         logger.info("wrote script for submitting jobs to %s", script_path)
+
+
+def run_workflow(work_folder, result_folder, data_folder, data_filename_pattern, job_count,
+                 sample_factor, extra_args_subsample="", extra_args_learn="",
+                 extra_args_apply_weights="", extra_args_score="", callback=None, logger=None):
+
+    result_folder, work_folder = _setup_folders(result_folder, work_folder, logger)
+
+    _write_script(work_folder, logger, settings=locals())
 
     if callback is not None:
         def inner():
@@ -65,5 +75,6 @@ def run_workflow(work_folder, result_folder, data_folder, data_filename_pattern,
     else:
         if logger is not None:
             logger.info("run workflow script")
-        out = subprocess.check_output("bash run.sh", shell=True, cwd=work_folder, stderr=subprocess.STDOUT)
+        out = subprocess.check_output("bash run.sh", shell=True, cwd=work_folder,
+                                      stderr=subprocess.STDOUT)
         return out, result_folder, work_folder
