ca9fc1fc4e686e2fcbf45c95332f0603125ec4dc
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-09-23 15:23:40 +0200

    SSDM-2425 : release 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from setuptools import setup
 
 setup(name="pyprophet-brutus-driver",
-      version="0.0.7",
+      version="0.0.8",
       author="Uwe Schmitt",
       author_email="uwe.schmitt@id.ethz.ch",
       license="BSD",
