51960ac27578a38f3735158c290013955576eb2a
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2015-11-16 16:36:59 +0100

    SSDM-2810 : release 0.0.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from setuptools import setup, find_packages
 
 setup(name="pyprophet-brutus",
-      version="0.0.15",
+      version="0.0.16",
       author="Uwe Schmitt",
       author_email="uwe.schmitt@id.ethz.ch",
       license="BSD",
